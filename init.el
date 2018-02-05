2984052fb3bfb3b4b8d868e5c23b73ed39f2bc9e
Nurdan <nurdann@users.noreply.github.com>
2018-02-04 23:37:59 -0800

    Added copy line

--- init.el
+++ init.el
@@ -9,8 +9,8 @@
 ;;(package-refresh-contents)
 
 ;; Load theme if in GUI
-(setq custom-safe-themes t)
-(when (display-graphic-p)
+(setq custom-safe-themes t) ;; skip prompt
+(when (display-graphic-p) ;; Enable in GUI
   (load-theme 'dracula t)
   )
 
@@ -20,15 +20,28 @@
 (tool-bar-mode -1)
 (global-linum-mode 1)
 (show-paren-mode 1)
+(global-set-key (kbd "C-x C-k") 'kill-buffer)
+(global-set-key (kbd "C-x f") 'find-file) ;; override fill column
 
+;; Copy current line
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+		  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s"))
+  )
+(global-set-key "\C-c\C-k" 'copy-line)
 
 ;; TYPING ;;
 
 (electric-pair-mode 1)
-(setq electric-pair-pairs '(
+(setq electric-pair-pairs '( ;; Additional pairs
 			    (?\" . ?\")
 			    (?\{ . ?\})
 			    ))
+
+
 
 ;; PACKAGES ;;
 
@@ -36,4 +49,5 @@
 (global-set-key [f8] 'neotree-toggle)
 
 (require 'window-number)
-(window-number-mode 1)
+(window-number-mode 1) ;; C-x C-j #
+(window-number-define-keys window-number-mode-map "C-x j") ;; avoid mistyping

7aa017c6bf36202bcf023a7100b799a22ff15141
Nurdan <nurdann@users.noreply.github.com>
2018-05-02 22:20:11 -0700

    Added magit and easymotion

--- init.el
+++ init.el
@@ -1,32 +1,28 @@
 
 (require 'package)
 (package-initialize)
-(setq package-archives '(;("gnu" . "https://elpa.gnu.org/packages/")
-                         ;("marmalade" . "https://marmalade-repo.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ))
-
-;;(package-refresh-contents)
+(add-to-list 'package-archives
+			 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-refresh-contents)
 
 ;; Load theme if in GUI
 (setq custom-safe-themes t) ;; skip prompt
 (when (display-graphic-p) ;; Enable in GUI
-  (load-theme 'dracula t)
-  )
+(load-theme 'dracula t))
 
 ;; LIFE QUALITY ;;
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (global-linum-mode 1)
-(show-paren-mode 1)
-(global-set-key (kbd "C-x C-k") 'kill-buffer)
+
 (global-set-key (kbd "C-x f") 'find-file) ;; override fill column
 
 (global-set-key (kbd "C-c C-S-k") (lambda () (interactive) (copy-lines 1)))
 (global-set-key (kbd "C-c C-k") 'copy-current-line)
 
 ;; Parentheses
+(show-paren-mode 1) ;; M-(
 (global-set-key (kbd "M-[") 'insert-pair)
 (global-set-key (kbd "M-{") 'insert-paren)
 
@@ -35,8 +31,8 @@
   "Copy lines (as many as prefix argument) in the kill ring"
   (interactive "p")
   (kill-ring-save (line-beginning-position)
-		  (line-beginning-position (+ 1 arg))
-		  )
+				  (line-beginning-position (+ 1 arg))
+				  )
   (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
 
 (defun copy-current-line ()
@@ -53,14 +49,14 @@
 
 ;; TYPING ;;
 
-(setq tab-width 4)
+(electric-indent-mode)
+(setq tab-width 2)
 (electric-pair-mode 1)
 (setq electric-pair-pairs '( ;; Additional pairs
 			    (?\" . ?\")
 			    (?\{ . ?\})
+			    ;;(?\` . ?\`)
 			    ))
-
-
 
 ;; PACKAGES ;;
 
@@ -68,5 +64,10 @@
 (global-set-key [f8] 'neotree-toggle)
 
 (require 'window-number)
-(window-number-mode 1) ;; C-x C-j #
-(window-number-define-keys window-number-mode-map "C-x j") ;; avoid mistyping
+(window-number-mode 1) ;; C-x C-j default
+(window-number-define-keys window-number-mode-map "<f4>")
+
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-SPC") 'ace-jump-mode)
+
+(global-set-key (kbd "C-x g") 'magit-status)

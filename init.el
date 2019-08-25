fa4d83ac5c84549c30a53ee8573a967a3c71467f
Nurdan <nurdann@users.noreply.github.com>
2019-08-24 20:00:42 -0700

    Added use-package, helm

--- init.el
+++ init.el
@@ -1,117 +1,101 @@
+;; C-h c <command> to get output of command sequence
 
 (require 'package)
 
 (add-to-list 'package-archives
-			 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+			 '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;(package-initialize)
+;(package-refresh-contents)
 
-(package-initialize)
-;;(package-refresh-contents)
+(eval-when-compile (require 'use-package))
 
-;; Load theme if in GUI
-(setq custom-safe-themes t) ;; skip prompt
-(when (display-graphic-p) ;; Enable in GUI
-(load-theme 'dracula t))
+;(setq custom-safe-themes t) ;; skip prompt
+;(when (display-graphic-p)
+;  (load-theme 'dracula t))
 
-;; LIFE QUALITY ;;
+(use-package circadian
+  :config
+  (setq circadian-themes '(("8:00" . creamsody)
+			   ("19:00" . parchment)))
+  (circadian-setup))
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Misc
+;;;;;;;;;;;;;;;;;;;;
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (global-linum-mode 1)
+;(setq cursor-type '(hbar . 4))
 
-(setq cursor-type '(hbar . 4))
-
-(global-set-key (kbd "C-x f") 'find-file) ;; override fill column
-
-(global-set-key (kbd "C-c C-S-k") (lambda () (interactive) (copy-lines 1)))
+(global-unset-key (kbd "C-z"))
+		  
 (global-set-key (kbd "C-c C-k") 'copy-current-line)
 
-;; Parentheses
-(show-paren-mode 1) ;; M-(
-(global-set-key (kbd "M-[") 'insert-pair)
-(global-set-key (kbd "M-{") 'insert-paren)
+;;;;;;;;;;;;;;;;;;;;
+;; Editing
+;;;;;;;;;;;;;;;;;;;;
 
-;; Copy current line with new line
-(defun copy-lines (arg)
-  "Copy lines (as many as prefix argument) in the kill ring"
-  (interactive "p")
-  (kill-ring-save (line-beginning-position)
-				  (line-beginning-position (+ 1 arg))
-				  )
-  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+(show-paren-mode 1)
+;(global-set-key (kbd "M-[") 'insert-pair)
+;(global-set-key (kbd "M-{") 'insert-paren)
+
+;; Allow undo/redo
+;; Default C-c left/right arrow
+(use-package winner
+  :config (winner-mode 1)
+  :bind (("C-c <left>" . undo)
+	 ("C-c <right>" . redo)))
+
+;(electric-indent-mode)
+(use-package electric
+  :config
+  (electric-pair-mode 1)
+  (setq electric-pair-pairs '((?\" . ?\")
+			      (?\{ . ?\}))))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Navigating 
+;;;;;;;;;;;;;;;;;;;;
+
+(use-package ace-jump-mode
+  :bind ("C-." . ace-jump-mode))
+
+(windmove-default-keybindings) ;; Shift <arrow-key> to move windows
+
+(require 'window-numbering)
+(window-numbering-mode 1)
+
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+	 ("C-x f" . helm-find-files)
+	 ("C-x b" . helm-buffers-list)
+	 ("C-x C-f" . helm-recentf)
+	 :map helm-find-files-map
+	 ("DEL" . helm-find-files-up-one-level)))
+
+
+;; Matlab
+;(autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
+;(add-to-list 'auto-mode-alist '("\\.m$" . matlab-mode))
+;(setq matlab-indent-function t)
+;(setq matlab-shell-command "matlab")
+;
+;(add-to-list 'load-path "~/.emacs.d/packages/ematlab")
+;(load-library "matlab")
+;
+;(define-key matlab-mode-map (kbd "C-c l") 'matlab-shell-run-cell)
+;(define-key matlab-mode-map (kbd "C-c C-l") 'matlab-shell-run-region)
+;(define-key matlab-mode-map (kbd "C-S-l") 'matlab-shell-save-and-go)
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Functions
+;;;;;;;;;;;;;;;;;;;;
 
 (defun copy-current-line ()
   (interactive)
   (kill-ring-save (line-beginning-position)
 				  (line-end-position))
   (message "Copied current line"))
-
-(defun insert-paren ()
-  (interactive)
-  (if (region-active-p)
-      (insert-pair 1 ?{ ?})
-     (backward-paragraph)))
-
-;; Allow undo/redo
-;; Default C-c left/right arrow
-(winner-mode 1)
-
-;; TYPING ;;
-
-;; auto insert pairs of (), []
-(electric-indent-mode)
-
-(electric-pair-mode 1)
-
-;; Additional pairs
-(setq electric-pair-pairs '(
-			    (?\" . ?\")
-			    (?\{ . ?\})
-			    ;;(?\` . ?\`)
-					))
-
-;; auto complete
-(ac-config-default)
-(setq ac-ignore-case nil)
-
-;; Navigating ;;
-
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-
-;(require 'window-number)
-;(window-number-mode 1)
-;; C-x C-j default
-;; remap it to another key
-;(window-number-define-keys window-number-mode-map "<f4>")
-
-(require 'ace-jump-mode)
-(define-key global-map (kbd "C-SPC") 'ace-jump-mode)
-
-;; To show files on buffer
-(require 'ido)
-(ido-mode 1)
-(setq ido-default-buffer-method 'selected-window)
-(setq ido-default-file-method 'selected-window)
-;; Move between windows
-;; Shift <arrow-key>
-(windmove-default-keybindings)
-
-
-(require 'window-numbering)
-(window-numbering-mode 1)
-
-;; Version Control ;;
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; Matlab
-(autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
-(add-to-list 'auto-mode-alist '("\\.m$" . matlab-mode))
-(setq matlab-indent-function t)
-(setq matlab-shell-command "matlab")
-
-(add-to-list 'load-path "~/.emacs.d/packages/ematlab")
-(load-library "matlab")
-
-(define-key matlab-mode-map (kbd "C-c l") 'matlab-shell-run-cell)
-(define-key matlab-mode-map (kbd "C-c C-l") 'matlab-shell-run-region)
-(define-key matlab-mode-map (kbd "C-S-l") 'matlab-shell-save-and-go)

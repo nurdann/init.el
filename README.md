b5de1fe11689f7eb0603bf5c7aed0cf1846162da
Nurdan <nurdann@users.noreply.github.com>
2019-11-29 16:47:46 -0800

    added guide

---
+++ README.md
@@ -0,0 +1,94 @@
+# Emacs
+
+# Basics
+
+## Notation
+
+Mnemonics - makes sense but kinda horrible
+
+For example, the combination `C-n` (hold Control and press n) moves cursor to the next line and `C-p` to the previous line.
+
+- `C` stand for Control
+- `M` (meta) stands for Alt
+- `S` for Shift
+- `s` for Super or windows
+
+You can also have combinations such as `C-M-x` (hold Control and Alt, then press x). 
+
+Note that Emacs will try to map shifted keys to regular keys if they are not bound, so for example `M-x` and `M-X` map to the same thing, i.e. `execute-extended-command`.
+
+`C-x C-f` is two combinations is sequence, so press `C-x` first (you will see it minibuffer as `C-x-`) and then press `C-f`. 
+**DO NOT** try to hold Control while trying to press `x` and `f` in sequence because it puts too much strain on your fingers and can result in RSI (Repetitive-Strain-Injury) Syndrom. Or even better is to remap frequent keys into dedicated keys or use fewer key strokes.
+
+## Movement
+
+- `C-n` move down a line
+- `C-p` move up a line
+- `C-f` move forward a character
+- `C-b` move backward a character
+- `M-f` move forward a word (alphanumeric sequence)
+- `M-b` move backward a word
+- `C-x C-f` find file and open
+- `C-x d` dired or show directory
+- `C-x b` show buffers which are usually currently opened files
+- `C-x C-c` exit emacs
+- `C-x C-s` save file
+
+- `C-a` move beginning of line
+- `C-e` move end of line
+- `C-s` search forward, keep pressing `C-s` to go to next match
+  - you can either press `<enter>` to stay on highlighted match or press `C-g` to back to original position
+- `C-r` search backward, same as above but use `C-r` to go previous match
+- `C-w` cut or in Emacs lingo "kill region"
+- `M-w` copy to kill ring
+- `C-y` paste from kill ring
+  - `M-y` cycle paste from kill ring
+- `M-x` execute command
+  - search for either dumb terminal `shell` or `term` which behaves same as regular terminal
+  - Note: inside `term`, the prefix command `C-x` switches to `C-c`, so to send an actual `C-c` to the terminal type it twice, i.e. `C-c C-c`.
+
+
+## Emacs vs Vim
+
+`vi` is archaic version of Vim. With Vim you usually edit files and go back to terminal but with Emacs you try to do everything inside it.
+
+There is a compromise called `evil-mode` which let's you use Vim keybinding.
+
+## key bindings
+-
+`C`
+`C-h c` describe key
+
+## Files or Dired
+
+### abbrev(iation)
+
+```
+(add-to-list
+    'directory-abbrev-alist
+    '("^/jou" . "~/mnt/mdbackup/journal" ))
+```
+Then type `C-x C-f /jou <RET>`
+
+### Bookmark files
+`C-x r m` mark for bookmark
+`C-x r b` prompt for saved bookmarks
+
+
+### ssh
+
+```
+# ~/.ssh/config
+
+Host goo
+     HostName google.ca
+     User root
+``` 
+Then type `C-x C-f /ssh:goo:/` to get password prompt 
+
+#### sudo or TRAMP 
+
+Type `C-x C-f /sudo:user@localhost:`
+
+
+
---
+++ intro.el
@@ -0,0 +1,100 @@
+;; `C-n` move down a line
+;; `C-p` move up a line
+;; `C-f` move forward a character
+;; `C-b` move backward a character
+;; `M-f` move forward a word (alphanumeric sequence)
+;; `M-b` move backward a word
+;; `C-x C-f` find file and open
+;; `C-x d` dired or show directory
+;; `C-x b` show buffers which are usually currently opened files
+;; `C-x C-c` exit emacs
+;; `C-x C-s` save file
+;; `C-x k` kill buffer
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+;; go to line with (add-to-list ...)
+;; and press `C-M-x` to evaluate lisp function; or move cursor the end of parenthesis and evaluate last expression with `C-x C-e`
+;; then go to command menu `M-x` and type `list-packages` now you will see all available packages for download
+
+;; `S-<arrow-key>`, Shift <arrow-key> to move around windows
+;; Some terminology: frame contains windows, so window is the buffer window in Emacs 
+(windmove-default-keybindings) 
+;; If you don't want to use them, then use default `C-x o` to cycle through windows
+
+;; change default behaviour
+(setq ring-bell-function 'ignore ;; disable sound bell on error
+      select-enable-clipboard t ;; copy/cut kill-ring to clipboard
+      read-buffer-completion-ignore-case t
+      read-file-name-completion-ignore-case t 
+      indent-tabs-mode nil
+      tab-width 4
+      set-mark-command-repeat-pop t ;; After C-u C-SPC, C-SPC cycles through the mark ring
+      mark-ring-max 16 
+      window-combination-resize t
+)
+
+;; save customization done via Emacs interface to different
+;; folder, otherwise Emacs will append to init file 
+(setq custom-file "~/.emacs.d/custom.el")
+(if (file-exists-p custom-file)
+    (load custom-file))
+;; Use `C-h` prefix to get help
+;; `C-h f` describe function
+;; `C-h v` describe variable
+;; `C-h c` describe key sequence
+;; `C-h m` describe mode
+(menu-bar-mode 1)
+(tool-bar-mode -1)
+;; you can set fill-column value with `C-x f`
+(auto-fill-mode -1)
+(show-paren-mode 1)
+(electric-pair-mode 1)
+;; add additional pairs for quotation and curly braces
+(setq electric-pair-pairs '((?\" . ?\")
+			    (?\{ . ?\})))
+
+;; C-x combo is very common so I remap it to a single key
+(global-set-key (kbd "<menu>") ctl-x-map)
+;; Then you either set global keybinding or add to a mode map
+(define-key ctl-x-map (kbd "f") 'find-file)
+;; OR 
+(global-set-key (kbd "C-x f") 'find-file)
+
+;; sudo
+;; `C-x C-f /sudo::/` press <tab> to get prompt
+;; `C-x C-f /ssh:user@host:/`
+
+;; IDO mode
+;; enhances behaviour of find-file and switch-buffer
+(ido-mode 1)
+(setq ido-enable-flex-matching t
+      ido-everywhere t
+      ido-auto-merge-work-directories-length -1
+      ido-use-virtual-buffers t)
+
+
+
+;; Dired
+(setq dired-listing-switches "-alh")
+(setq dired-auto-revert-buffer t)
+;; Go to Dired with `C-x d`
+
+;; Find file C
+(ido-mode 1)
+
+;; CUA mode
+
+;; Evil mode
+
+;; use-package
+
+;; chords
+;;(key-chord-define-global k c)
+
+;; jump to minimum strokes
+;; avy
+

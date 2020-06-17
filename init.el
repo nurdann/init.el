a96a034aab8ad0387db98b1ae7e32a5a0e7b6901
Nurdan <nurdann@users.noreply.github.com>
2020-06-17 11:11:51 -0700

    added TAGS to haskell-mode

--- init.el
+++ init.el
@@ -306,6 +306,8 @@
 ;; https://ebzzry.io/en/emacs-pairs/
 ;; https://github.com/Fuco1/smartparens/wiki/Hybrid-S-expressions
 ;; https://github.com/Fuco1/smartparens/wiki/Permissions#insertion-specification
+
+;; use C-q <any-bracket> to insert a single character otherwise Emacs freezes
 (use-package smartparens
   :ensure t
   :config (smartparens-global-mode 1)
@@ -502,19 +504,28 @@
   ;;(setq-default haskell-process-type 'cabal-repl)
 
   ;; http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html#Interactive-Haskell
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (custom-set-variables
+  ;;'(haskell-process-suggest-remove-import-lines t)
+  ;;'(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t)
 
-  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  ;;'(haskell-process-type 'cabal-repl)
+
+  ;; cabal install hasktags (make sure `hasktags` in PATH)
+  ;; M-x visit-tags-table (Manually select TAGS file)
+  '(haskell-tags-on-save t) 
   )
+
+  :bind (
+         :map haskell-interactive-mode-map
+         ("C-c C-k" . nil)
+         ("M-." . haskell-mode-jump-to-def-or-tag)
+         ("C-c f" . haskell-goto-first-error)
+         ("<up>" . haskell-interactive-mode-history-previous)
+         ("<down>" . haskell-interactive-mode-history-next)
+         :map haskell-cabal-mode-map
+         
+         ))
 
 (use-package hindent
   :ensure t
